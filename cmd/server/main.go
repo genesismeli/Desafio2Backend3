ecd30f704559ac9fa0afbb11262ffa98e5571dc3
StefanySB <stefany9209@gmail.com>
2023-09-19 09:26:20 -0500

    requerimientos de los cuatro metodos solicitados y probados ok

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
-	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 
@@ -40,49 +37,6 @@ func main() {
 	PacientesGroup.POST("/create", controlador.Create())
 	PacientesGroup.PUT("/:id", controlador.Update())
 	PacientesGroup.DELETE("/:id", controlador.Delete())
-	PacientesGroup.PATCH("/:id", func(c *gin.Context) {
-		userID := c.Param("id")
-		userIDint, err := strconv.Atoi(userID)
-
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		var userReq pacienteModel.RequestPaciente2
-
-		err = c.BindJSON(&userReq)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		backgroundContext := context.Background()
-		paciente, _ := pacienteService.GetByID(backgroundContext, userIDint)
-
-		if userReq.Nombre != nil {
-			paciente.Nombre = *userReq.Nombre
-		}
-
-		if userReq.Apellido != nil && *userReq.Apellido != "" {
-			paciente.Apellido = *userReq.Apellido
-		}
-
-		if userReq.Domicilio != nil {
-			paciente.Domicilio = *userReq.Domicilio
-		}
-
-		if userReq.DNI != nil {
-			paciente.DNI = *userReq.DNI
-		}
-
-		if userReq.FechaAlta != nil {
-			paciente.FechaAlta = *userReq.FechaAlta
-		}
-
-		paciente, _ = pacienteDatabase.Update(backgroundContext, paciente)
-		c.JSON(http.StatusOK, paciente)
-	})
-
 	PacientesGroup.PATCH("/patch/:id", controlador.UpdateField())
 
 	router.Run("localhost" + puerto)
