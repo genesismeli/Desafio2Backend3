fd0a4e385f586b54f6884e2fa893af52661f41bf
lenahu12 <nahuelquintana97@gmail.com>
2023-09-19 02:43:49 -0300

    imprementacion del middleware de autenticacion, usando godotenv

--- core/middleware/security.go
+++ core/middleware/security.go
@@ -1,1 +1,24 @@
 package middleware
+
+import (
+	"net/http"
+	"os"
+	"github.com/gin-gonic/gin"
+)
+
+// middleware de autenticación, validará el token recibido en el header (debe coincidir con: testAdminToken321), 
+// con el token almacenado en el archivo .env
+// go get github.com/joho/godotenv 
+
+func Authenticate() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		adminToken := os.Getenv("adminToken")
+		tokenHeader := ctx.GetHeader("token")
+		if tokenHeader != adminToken {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"mensaje": "Usuario No autenticado como Administrador.",
+			})
+		} 
+		ctx.Next()
+	}
+}
